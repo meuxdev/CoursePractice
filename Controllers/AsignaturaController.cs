cb63d6323743a2e443accc35984a4162c1e36593
Alejandro Andrade <meuxdev@gmail.com>
2022-07-21 15:17:59 -0500

    Partial Views

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using CoursePractice.Models;
+using System.Collections.Generic;
 
 namespace CoursePractice.Controllers
 {
@@ -8,12 +9,42 @@ namespace CoursePractice.Controllers
     {
         public IActionResult Index()
         {
-            var assingment = new Asignatura()
+            return View(new Asignatura()
             {
-                Nombre = "Math",
+                Nombre = "Programacion con .NET",
                 UniqueId = Guid.NewGuid().ToString(),
+            });
+        }
+
+        public IActionResult All()
+        {
+
+            var listaAsignaturas = new List<Asignatura>() {
+                new Asignatura {
+                    Nombre = "Matemáticas",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                    Nombre = "Educación Física",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                    Nombre = "Castellano",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                    Nombre = "Ciencias Naturales",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                    Nombre = "Programacion",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                }
             };
-            return View(assingment);
+
+            ViewBag.Date = DateTime.Now;
+
+            return View(listaAsignaturas);
         }
     }
 }
