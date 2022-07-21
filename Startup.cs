4f50b426a164baf5c9a6792e0303fed6686b12bf
Alejandro Andrade <meuxdev@gmail.com>
2022-07-21 13:33:44 -0500

    Razor syntax.

--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,7 @@ namespace CoursePractice
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=School}/{action=Index}/{id?}");
             });
         }
     }
